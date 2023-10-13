a4340fa79a3ae6f4f2d35f201678354c0e50e1b6
mongragong <a06729@naver.com>
2023-10-13 12:12:38 +0900

    동영상 다운로드 기능 구현

--- src/preload.ts
+++ src/preload.ts
@@ -1,7 +1,13 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-const { contextBridge, ipcRenderer } = require('electron')
+const { contextBridge, ipcRenderer} = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
   setTitle: (title:any) => ipcRenderer.send('set-title', title)
+});
+
+contextBridge.exposeInMainWorld('file', {
+  openDirectory:async (fileName:string,fileUrl:string) => {
+    ipcRenderer.send('openDirectory',fileName,fileUrl);
+  }
 })
--- src/view/page/main.tsx
+++ src/view/page/main.tsx
@@ -1,20 +1,45 @@
+import Button from '@mui/material/Button';
+import React, { useState } from 'react';
 declare global {
     interface Window {
         electronAPI:any;
+        file:any;
     }
 }
 
+
+
+
 function Main(){
+    const [fileName, setfileName] = useState('');
+    const [fileUrl,setFileUrl]=useState('');
+    const fileNameChange=(e: React.FormEvent<HTMLInputElement>)=>{
+        const fileName=e.currentTarget.value;
+        
+        console.log(e.currentTarget.value);
+        
+        setfileName(fileName);
+    }
+    const fileUrlChange=(e: React.FormEvent<HTMLInputElement>)=>{
+        const fileUrl=e.currentTarget.value;
+        console.log(fileUrl);
+        setFileUrl(fileUrl);
+    }
+
     return(
         <div>
-            <input type="text" placeholder="이름" />
-            <button onClick={clickfnc}>버튼</button>
+            <input type="text" onChange={fileNameChange} placeholder="파일이름" />
+            <input type="text" onChange={fileUrlChange} placeholder='url주소'/>
+            <Button variant="outlined" onClick={()=>{
+                clickfnc(fileName,fileUrl);
+            }}>다운로드</Button>
         </div>
     );
 }
 
-function clickfnc(){
-    window.electronAPI.setTitle("test")
+
+function clickfnc(fileName:string,fileUrl:string){
+    window.file.openDirectory(fileName,fileUrl);
 }
 
 export default Main;
